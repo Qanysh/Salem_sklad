51663e043ee06cf46099e11dc0268290668a6673
Qanysh <220667@astanait.edu.kz>
2024-05-15 14:53:07 +0500

    admin and auth

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const path = require('path');
+const ensureIsAdmin = require('./middleware/AuthMiddleware');
 
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
+
 const imagesPath = path.join(__dirname, '/views'); // Путь к папке с изображениями
 
+app.set('view engine', 'ejs');
 app.use(express.static(imagesPath)); // Используем express.static для обслуживания статических файлов из папки с изображениями
+app.use(express.json());
 
+app.get('/admin', ensureIsAdmin, async (req, res) => {
+    const user = await User.findById(req.session.userId);
+    res.render('admin/admin');
+});
 
 app.get('/', (req, res) => {
-    // Отправляем файл index.html
     res.render('index');
 });
 
---
+++ middleware/AuthMiddleware.js
@@ -0,0 +1,22 @@
+const { User } = require('../database/db');
+
+async function ensureIsAdmin(req, res, next) {
+    if (req.session && req.session.userId) {
+        try {
+            const user = await User.findById(req.session.userId);
+
+            if (user && user.admin) {
+                next();
+            } else {
+                res.redirect('/');
+            }
+        } catch (error) {
+            console.error("Error checking admin status:", error);
+            res.redirect('/');
+        }
+    } else {
+        res.redirect('/');
+    }
+}
+
+module.exports = ensureIsAdmin;
