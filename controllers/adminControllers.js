5071b1ba18f6a4bd4f9c920446b0aecec0e6f8ec
Qanysh <220667@astanait.edu.kz>
2024-05-21 00:02:36 +0500

    add box-descriptions and fix admin panel

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,9 +1,10 @@
 const { User, Client } = require('../database/db');
+const moment = require("moment");
 
 async function adminControllerGetUsers(req, res) {
     const admin = await User.findById(req.session?.userId);
     const clients = await Client.find();
-    res.render('admin/admin', { user: admin, allClients: clients, error: null });
+    res.render('admin/admin', { user: admin, allClients: clients, error: null, moment: moment });
 }
 
 module.exports = { adminControllerGetUsers };
